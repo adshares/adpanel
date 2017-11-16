75215fd99cc9c12c5ac6dec5d3d73d721bc7983c
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-11-17 00:31:12 +0100

    Created styles for registration, login and confirmation

--- src/app/auth/confirmation/confirmation.component.ts
+++ src/app/auth/confirmation/confirmation.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class ConfirmationComponent implements OnInit {
+  email: string = 'mateuszpiatek@10clouds.com'
 
   constructor() { }
 
