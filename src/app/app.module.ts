ccb8829579f6dc9d894b8318b30825ceaa4354c5
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-11-17 13:44:57 +0100

    Added AvenirNext fonts and updated weights, added animations for router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { AdvertiserModule } from './advertiser/advertiser.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule
     AppRoutingModule,
     AuthModule,
     AdvertiserModule
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fadeAnimation } from '../animations/fade.animation';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
+  animations: [fadeAnimation]
 })
-export class AuthComponent implements OnInit {
 
+export class AuthComponent implements OnInit {
+  testState = 'normal';
   constructor() { }
 
   ngOnInit() {
   }
 
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
+  }
 }
