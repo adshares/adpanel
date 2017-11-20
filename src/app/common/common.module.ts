4ef18d7f00333c92d25df5dfa867091e55143f57
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-11-20 13:52:50 +0100

    Created common module and moved BrowserAnimationsModule there

---
+++ src/app/common/common.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NotificationsComponent } from './components/notifications/notifications.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule
+  ]
+})
+
+export class AppCommonModule { }
