329a95b675e40b171d88fe4c1a732d6915dadd99
KonradFak <konrad.fak@10clouds.com>
2017-11-27 11:08:58 +0100

    Import only needed operator in auth.service.js

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 import { environment } from '../../environments/environment';
 
