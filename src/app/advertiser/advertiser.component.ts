e02d7179e6a0a0990ab9b7291b752d6e76d79ba5
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-11-29 15:08:23 +0100

    Added animations to router-outlets

--- src/app/advertiser/advertiser.component.ts
+++ src/app/advertiser/advertiser.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from "../common/animations/fade.animation";
 
 @Component({
   selector: 'app-advertiser',
   templateUrl: './advertiser.component.html',
-  styleUrls: ['./advertiser.component.scss']
+  styleUrls: ['./advertiser.component.scss'],
+  animations: [fadeAnimation]
+
 })
 export class AdvertiserComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
   }
-
 }
--- src/app/advertiser/edit-campaign/edit-campaign.component.ts
+++ src/app/advertiser/edit-campaign/edit-campaign.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from "../../common/animations/fade.animation";
 
 @Component({
   selector: 'app-edit-campaign',
   templateUrl: './edit-campaign.component.html',
   styleUrls: ['./edit-campaign.component.scss'],
+  animations: [fadeAnimation],
 })
 export class EditCampaignComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
   }
-
 }
