dd99a563e3692590d3f21316a707b6f4dfd98f80
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-11-29 20:04:00 +0100

    Adjusted indents in HTML files

--- src/app/advertiser/edit-campaign/edit-campaign.component.ts
+++ src/app/advertiser/edit-campaign/edit-campaign.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component  } from '@angular/core';
 
 @Component({
   selector: 'app-edit-campaign',
   templateUrl: './edit-campaign.component.html',
   styleUrls: ['./edit-campaign.component.scss'],
 })
-export class EditCampaignComponent implements OnInit {
+export class EditCampaignComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
