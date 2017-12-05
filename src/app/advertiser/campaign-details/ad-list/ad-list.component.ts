0ad367944a58613cb223990797a45dd344fe5367
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-12-05 13:46:51 +0100

    Shorter version of campaign list and ad list navigation

--- src/app/advertiser/campaign-details/ad-list/ad-list.component.ts
+++ src/app/advertiser/campaign-details/ad-list/ad-list.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ad-list',
   templateUrl: './ad-list.component.html',
   styleUrls: ['./ad-list.component.scss'],
 })
-export class AdListComponent implements OnInit {
+export class AdListComponent {
   @Input() adList: Object;
+  menuItems = [
+    { title: 'Status', columnWidth: 'col-xs-1' },
+    { title: 'Campaign Title', columnWidth: 'col-xs-4' },
+    { title: 'Budget', columnWidth: 'col-xs-1' },
+    { title: 'Clicks', columnWidth: 'col-xs-1' },
+    { title: 'Impressions', columnWidth: 'col-xs-1' },
+    { title: 'CTR', columnWidth: 'col-xs-1' },
+    { title: 'Average CPC', columnWidth: 'col-xs-1' },
+    { title: 'Cost', columnWidth: 'col-xs-1' },
+  ];
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
--- src/app/advertiser/campaign-list/campaign-list.component.ts
+++ src/app/advertiser/campaign-list/campaign-list.component.ts
@@ -10,6 +10,17 @@ import { AppState } from '../../models/app-state.model';
 })
 
 export class CampaignListComponent {
+  menuItems = [
+    { title: 'Status', columnWidth: 'col-xs-1' },
+    { title: 'Campaign Title', columnWidth: 'col-xs-4' },
+    { title: 'Budget', columnWidth: 'col-xs-1' },
+    { title: 'Clicks', columnWidth: 'col-xs-1' },
+    { title: 'Impressions', columnWidth: 'col-xs-1' },
+    { title: 'CTR', columnWidth: 'col-xs-1' },
+    { title: 'Average CPC', columnWidth: 'col-xs-1' },
+    { title: 'Cost', columnWidth: 'col-xs-1' },
+  ];
+
   private subscription;
   campaigns: Object;
 
