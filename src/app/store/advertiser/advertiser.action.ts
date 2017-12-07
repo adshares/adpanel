9bebda075240c56e98105964f94cbcdabc642540
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-12-07 08:56:44 +0100

    Changed message for action

--- src/app/store/advertiser/advertiser.action.ts
+++ src/app/store/advertiser/advertiser.action.ts
@@ -1,12 +1,11 @@
 import { Action } from '@ngrx/store';
 
-export const SAVE_CAMPAIGN_BASIC_INFORMATION = 'SAVE_CAMPAIGN_BASIC_INFORMATION';
+export const SAVE_CAMPAIGN_BASIC_INFORMATION = 'Basic Campaign Information saved';
 export const LOAD_CAMPAIGNS = 'Campaigns loaded';
 export const LOAD_CAMPAIGNS_SUCCESS = 'Campaigns loaded success';
 
 export class SaveCampaignBasicInformation implements Action {
   readonly type = SAVE_CAMPAIGN_BASIC_INFORMATION;
-
   constructor(public payload: any) { };
 }
 
