eab9b128023a7e9d66bd78a55e80a2ab3efe017b
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-12-12 14:44:22 +0100

    First idea for navigation menu

--- src/app/advertiser/campaign-details/ad-list/ad-list.component.ts
+++ src/app/advertiser/campaign-details/ad-list/ad-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-ad-list',
   templateUrl: './ad-list.component.html',
@@ -7,14 +8,5 @@ import { Component, Input } from '@angular/core';
 })
 export class AdListComponent {
   @Input() adList: Object;
-  menuItems = [
-    { title: 'Status', columnWidth: 'col-xs-1' },
-    { title: 'Campaign Title', columnWidth: 'col-xs-4' },
-    { title: 'Budget', columnWidth: 'col-xs-1' },
-    { title: 'Clicks', columnWidth: 'col-xs-1' },
-    { title: 'Impressions', columnWidth: 'col-xs-1' },
-    { title: 'CTR', columnWidth: 'col-xs-1' },
-    { title: 'Average CPC', columnWidth: 'col-xs-1' },
-    { title: 'Cost', columnWidth: 'col-xs-1' },
-  ];
+
 }
