52546fddb68c188aba75a8132644cd22c66240a6
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-12-13 09:21:50 +0100

    Reusable navigation (ad list, campaign list). Potential for future navigations in settings, publisher, reports

---
+++ src/app/common/components/table-navigation/table-navigation.component.ts
@@ -0,0 +1,50 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-table-navigation',
+  templateUrl: './table-navigation.component.html',
+  styleUrls: ['./table-navigation.component.scss']
+})
+export class TableNavigationComponent implements OnInit {
+  @Input() navigationName;
+  navigationItems = [];
+
+  adListNavigationItems = [
+    { title: 'Status', columnWidth: 'col-xs-1' },
+    { title: 'Campaign Title', columnWidth: 'col-xs-4' },
+    { title: 'Budget', columnWidth: 'col-xs-1' },
+    { title: 'Clicks', columnWidth: 'col-xs-1' },
+    { title: 'Impressions', columnWidth: 'col-xs-1' },
+    { title: 'CTR', columnWidth: 'col-xs-1' },
+    { title: 'Average CPC', columnWidth: 'col-xs-1' },
+    { title: 'Cost', columnWidth: 'col-xs-1' },
+  ];
+
+  campaignListNavigationItems = [
+    { title: 'Status', columnWidth: 'col-xs-1' },
+    { title: 'Campaign Title', columnWidth: 'col-xs-4' },
+    { title: 'Budget', columnWidth: 'col-xs-1' },
+    { title: 'Clicks', columnWidth: 'col-xs-1' },
+    { title: 'Impressions', columnWidth: 'col-xs-1' },
+    { title: 'CTR', columnWidth: 'col-xs-1' },
+    { title: 'Average CPC', columnWidth: 'col-xs-1' },
+    { title: 'Cost', columnWidth: 'col-xs-1' },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.navigationName)
+    switch (this.navigationName) {
+      case 'adListNavigation':
+        this.navigationItems = this.adListNavigationItems;
+        break;
+      case 'campaignListNavigation':
+        this.navigationItems = this.campaignListNavigationItems;
+        break;
+    }
+
+    console.log(this.navigationItems);
+  }
+
+}
