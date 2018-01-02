1411da89c25de7d00549b1b46afaf7a1fe4a204a
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-01-02 11:27:41 +0100

    Added get code dialog and ad units

---
+++ src/app/publisher/site-details/ad-units/ad-units.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { SiteCodeDialogComponent } from '../../dialogs/site-code-dialog/site-code-dialog.component';
+
+@Component({
+  selector: 'app-ad-units',
+  templateUrl: './ad-units.component.html',
+  styleUrls: ['./ad-units.component.scss'],
+})
+export class AdUnitsComponent {
+  @Input() adUnit;
+
+  constructor(private dialog: MatDialog) { }
+
+  openGetCodeDialog() {
+    this.dialog.open(SiteCodeDialogComponent);
+  }
+
+}
