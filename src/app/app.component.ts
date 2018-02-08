8cede5e00f6244fb33dc3cae4c6b735ef7d1d210
KonradFak <konrad.fak@10clouds.com>
2018-02-08 12:55:48 +0100

    Handle auth token with saving user data to local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import { AppState } from './models/app-state.model';
+import { User, LocalStorageUser } from './models/user.model';
 import { fadeAnimation } from './common/animations/fade.animation';
 import { appSettings } from '../app-settings/app-settings';
+import * as authActions from './store/auth/auth.actions';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +16,15 @@ import { appSettings } from '../app-settings/app-settings';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private store: Store<AppState>
+  ) { }
 
   getRouterOutletState = (outlet) => outlet.isActivated ? outlet.activatedRoute : '';
 
   ngOnInit() {
+    this.handleSavedUserData();
     this.router.events.subscribe((event) => {
       if (!(event instanceof NavigationEnd)) {
         return;
@@ -23,5 +32,29 @@ export class AppComponent implements OnInit {
 
       setTimeout(() => window.scrollTo(0, 0), appSettings.ROUTER_TRANSITION_DURATION);
     });
+  }
+
+  handleSavedUserData() {
+    if (this.router.url.indexOf('auth') > -1) {
+      return;
+    }
+
+    const userData: LocalStorageUser = JSON.parse(localStorage.getItem('adshUser'));
+    const nowUnix = (+new Date) / 1000 | 0;
+
+    if (!userData) {
+      this.router.navigate(['/auth', 'login']);
+      return;
+    }
+
+    const user = (({id, email, isAdvertiser, isPublisher, isAdmin, authToken}) =>
+      ({id, email, isAdvertiser, isPublisher, isAdmin, authToken}))(userData);
+
+    if (userData.expiration < nowUnix) {
+      localStorage.removeItem('adshUser');
+      this.router.navigate(['/auth', 'login']);
+    } else {
+      this.store.dispatch(new authActions.SetUser(user));
+    }
   }
 }
