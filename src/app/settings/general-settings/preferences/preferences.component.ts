8d6f532d7ba1e56c535056715c52b7df602c42d9
KonradFak <konrad.fak@10clouds.com>
2018-02-11 22:46:36 +0100

    Added change email and password methods

--- src/app/settings/general-settings/preferences/preferences.component.ts
+++ src/app/settings/general-settings/preferences/preferences.component.ts
@@ -1,16 +1,36 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { SettingsService } from '../../settings.service';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HandleSubscription } from '../../../common/handle-subscription';
+
+interface AfterRequestValidation {
+  email: { [key: string]: boolean },
+  password: { [key: string]: boolean }
+};
 
 @Component({
   selector: 'app-preferences',
   templateUrl: './preferences.component.html',
   styleUrls: ['./preferences.component.scss']
 })
-export class PreferencesComponent extends HandleSubscription {
-  @ViewChild('changeEmailForm') changeEmailForm: NgForm;
-  @ViewChild('changePasswordForm') changePasswordForm: NgForm;
+export class PreferencesComponent extends HandleSubscription implements OnInit {
+  changeEmailForm: FormGroup;
+  changeEmailFormSubmitted = false;
+  changePasswordForm: FormGroup;
+  changePasswordFormSubmitted = false;
+
+  newPasswordConfirm = new FormControl();
+  afterRequestValidation: AfterRequestValidation = {
+    email: {
+      success: false,
+      emailChangeFailed: false
+    },
+    password: {
+      success: false,
+      wrongPreviousPassword: false,
+      passwordChangeFailed: false
+    }
+  };
 
   constructor(
     private settingsService: SettingsService
@@ -18,39 +38,77 @@ export class PreferencesComponent extends HandleSubscription {
     super(null);
   }
 
-  onChangeEmail(newEmail) {
+  ngOnInit() {
+     this.createForms();
+  }
+
+  createForms() {
+    this.changeEmailForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+
+    this.changePasswordForm = new FormGroup({
+      currentPassword: new FormControl('', Validators.required),
+      newPassword: new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  onChangeEmail() {
+    const newEmail = this.changeEmailForm.get('email').value;
+
+    this.changeEmailFormSubmitted = true;
+    Object.keys(this.afterRequestValidation.email).forEach(
+      (validation) => this.afterRequestValidation.email[validation] = false
+    );
+
     if (!this.changeEmailForm.valid) {
       return;
     }
 
-    // TODO
     const changeEmailSubscription = this.settingsService.changeEmail(newEmail)
-      .subscribe((email) => {
-        this.settingsService.changeEmail(email);
-      });
+      .subscribe(
+        () => {
+          this.changeEmailForm.get('email').setValue('');
+          this.afterRequestValidation.email.success = true;
+        },
+        () => this.afterRequestValidation.email.emailChangeFailed = true,
+        () => this.changeEmailFormSubmitted = false
+      );
     this.subscriptions.push(changeEmailSubscription);
-
-    this.changeEmailForm.value.email = '';
   }
 
-  onChangePassword(currentPassword, newPassword, newPasswordConfirm) {
-    if (!this.changePasswordForm.valid) {
+  onChangePassword() {
+    const currentPassword = this.changePasswordForm.get('currentPassword').value;
+    const newPassword = this.changePasswordForm.get('newPassword').value;
+
+    this.changePasswordFormSubmitted = true;
+    Object.keys(this.afterRequestValidation.password).forEach(
+      (validation) => this.afterRequestValidation.password[validation] = false
+    );
+
+    if (!this.changePasswordForm.valid || (this.newPasswordConfirm.value !== newPassword)) {
       return;
     }
 
-    // TODO
     const changePasswordSubscription = this.settingsService.changePassword(
       currentPassword,
-      newPassword,
-      newPasswordConfirm
+      newPassword
     )
-      .subscribe((email) => {
-        this.settingsService.changeEmail(email);
-      });
+      .subscribe(
+        () => {
+          this.changePasswordForm.reset();
+          this.newPasswordConfirm.setValue('');
+          this.afterRequestValidation.password.success = true;
+        },
+        (err) => {
+          if (err.code = 412) {
+            this.afterRequestValidation.password.wrongPreviousPassword = true;
+          } else {
+            this.afterRequestValidation.password.passwordChangeFailed = true;
+          }
+        },
+        () => this.changePasswordFormSubmitted = false
+      );
     this.subscriptions.push(changePasswordSubscription);
-
-    this.changePasswordForm.value.currentPassword = '';
-    this.changePasswordForm.value.newPassword = '';
-    this.changePasswordForm.value.newPasswordConfirm = '';
   }
 }
