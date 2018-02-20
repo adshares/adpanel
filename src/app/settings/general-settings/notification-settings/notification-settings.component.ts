8b655739e6c49d8dde0b3e7ecea9fe0edb444b03
KonradFak <konrad.fak@10clouds.com>
2018-02-20 14:15:08 +0100

    Fixed typos, fixed wrong comparision, added handle subscribe on notifications subscribe

--- src/app/settings/general-settings/notification-settings/notification-settings.component.ts
+++ src/app/settings/general-settings/notification-settings/notification-settings.component.ts
@@ -1,36 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
 
 import { AppState } from '../../../models/app-state.model';
+import { HandleSubscription } from '../../../common/handle-subscription';
+
 import { NotificationItem } from '../../../models/settings.model';
-import { Subscription } from 'rxjs/Subscription';
 import { SettingsService } from '../../settings.service';
 import { cloneDeep } from '../../../common/utilities/helpers';
 import * as settingsActions from '../../../store/settings/settings.actions';
-
-
 
 @Component({
   selector: 'app-notification-settings',
   templateUrl: './notification-settings.component.html',
   styleUrls: ['./notification-settings.component.scss']
 })
-export class NotificationSettingsComponent implements OnInit {
-  notificationSubscription: Subscription;
+export class NotificationSettingsComponent extends HandleSubscription implements OnInit {
   notificationsSettings: NotificationItem[];
 
   constructor(
     private store: Store<AppState>,
     private settingsService: SettingsService
   ) {
-    this.notificationSubscription = store
-      .select('state', 'user', 'settings', 'notificationsSettings')
-      .subscribe((notificationsSettings: NotificationItem[]) => {
-        this.notificationsSettings = notificationsSettings;
-      });
+    super(null);
   }
 
   ngOnInit() {
+    const notificationSubscription = this.store.select('state', 'user', 'settings', 'notificationsSettings')
+      .subscribe((notificationsSettings: NotificationItem[]) => {
+        this.notificationsSettings = notificationsSettings;
+      });
+    this.subscriptions.push(notificationSubscription);
+
     this.store.dispatch(new settingsActions.LoadNotificationsSettings(''));
   }
 
