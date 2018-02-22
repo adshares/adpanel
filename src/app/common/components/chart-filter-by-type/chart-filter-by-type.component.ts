4640854993b787e22b98e1b9e47fe32c6978b3f1
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-02-22 09:32:30 +0100

    Added filtering by type, id and series (only one value passed so far)

--- src/app/common/components/chart-filter-by-type/chart-filter-by-type.component.ts
+++ src/app/common/components/chart-filter-by-type/chart-filter-by-type.component.ts
@@ -1,4 +1,7 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { chartSeriesEnum } from '../../../models/enum/chart-series.enum';
+import { enumToArray } from '../../utilities/helpers';
 
 @Component({
   selector: 'app-chart-filter-by-type',
@@ -6,9 +9,18 @@ import {Component, EventEmitter, Output} from '@angular/core';
   styleUrls: ['./chart-filter-by-type.component.scss'],
 })
 export class ChartFilterByTypeComponent {
-  @Output() filter: EventEmitter<any> = new EventEmitter();
+  @Output() updateId: EventEmitter<any> = new EventEmitter();
+  @Output() updateSeries: EventEmitter<any> = new EventEmitter();
+  currentAssetId = 0;
+  currentAssetSeries = 'clicks';
 
-  filterChart(span) {
-    this.filter.emit(span);
+  chartSeries: string[] = enumToArray(chartSeriesEnum);
+
+  updateAssetId(arg) {
+    this.updateId.emit(arg);
+  }
+
+  updateAssetSeries(arg) {
+    this.updateSeries.emit(arg);
   }
 }
