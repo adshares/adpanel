8011ee57041a22867e2fbb64cc7d1debc3185a4f
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-02-26 09:37:52 +0100

    Added 4 dialogs for add/withdraw funds

---
+++ src/app/common/dialog/add-funds-dialog/add-funds-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-funds-dialog',
+  templateUrl: './add-funds-dialog.component.html',
+  styleUrls: ['./add-funds-dialog.component.scss']
+})
+export class AddFundsDialogComponent {
+
+}
---
+++ src/app/common/dialog/withdraw-funds-dialog/withdraw-funds-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-withdraw-funds-dialog',
+  templateUrl: './withdraw-funds-dialog.component.html',
+  styleUrls: ['./withdraw-funds-dialog.component.scss']
+})
+export class WithdrawFundsDialogComponent {
+
+}
