cc4ba2f340f2209f80d8b1c6ff9bdc97f82f8e6f
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-03-06 17:19:59 +0100

    Fixes from code review

--- src/app-settings/app-settings.ts
+++ src/app-settings/app-settings.ts
@@ -2,5 +2,7 @@ export const appSettings = {
   ROUTER_TRANSITION_DURATION: 200,
   MAX_AD_IMAGE_SIZE: 512000,
   REMEMBER_USER_EXPIRATION_SECONDS: 259200,
-  AUTH_TOKEN_EXPIRATION_SECONDS: 900
+  AUTH_TOKEN_EXPIRATION_SECONDS: 900,
+  PRIVACY_POLICY_LINK: 'https://adshares.net/privacy.txt',
+  FAQ_LINK: 'https://adshares.net/faq.html'
 };
