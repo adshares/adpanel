de5f1408580a7916972041d68893985cc0569651
KonradFak <konrad.fak@10clouds.com>
2018-03-06 14:11:09 +0100

    Add server mock to change admin settings

--- src/app/admin/dialogs/set-your-earnings-dialog/set-your-earnings-dialog.component.ts
+++ src/app/admin/dialogs/set-your-earnings-dialog/set-your-earnings-dialog.component.ts
@@ -1,29 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { HandleSubscription } from '../../../common/handle-subscription';
 import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+import { HandleSubscription } from '../../../common/handle-subscription';
 import { AppState } from '../../../models/app-state.model';
-import {AdminSettings} from "../../../models/settings.model";
+import { AdminSettings } from '../../../models/settings.model';
+import { AdminService } from '../../admin.service';
+import * as adminActions from '../../../store/admin/admin.actions';
 
 @Component({
   selector: 'app-set-your-earnings-dialog',
   templateUrl: './set-your-earnings-dialog.component.html',
   styleUrls: ['./set-your-earnings-dialog.component.scss']
 })
-export class SetYourEarningsDialogComponent extends HandleSubscription {
-  value = 0;
+export class SetYourEarningsDialogComponent extends HandleSubscription implements OnInit {
+  value: number;
+  valueChanged: Subject<number> = new Subject<number>();
 
   constructor(
     public dialogRef: MatDialogRef<SetYourEarningsDialogComponent>,
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private adminService: AdminService
   ) {
     super(null);
+  }
 
-    const adminSettingsSubscription = store.select('state', 'admin', 'settings')
+  ngOnInit() {
+    const adminStoreSettingsSubscription = this.store.select('state', 'admin', 'settings')
       .subscribe((settings: AdminSettings) => {
         this.value = settings.earnings * 100;
       });
+    this.subscriptions.push(adminStoreSettingsSubscription);
 
-    this.subscriptions.push(adminSettingsSubscription);
+    const addminSettingsChangeSubscription = this.valueChanged
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .subscribe((newValue) => {
+        this.adminService.setAdminSettings({ earnings: newValue })
+          .subscribe((newSettings) => this.store.dispatch(new adminActions.SetAdminSettings(newSettings)));
+       });
+    this.subscriptions.push(addminSettingsChangeSubscription);
+  }
+
+  updateValue(newValue) {
+    this.valueChanged.next(newValue);
   }
 }
