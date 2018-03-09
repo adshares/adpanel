d37dc0cd4f5fc20a7fe715e3afd036cd97fc472c
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-03-09 09:04:30 +0100

    Delted withdrawal amounts from enum

--- src/app/models/enum/withdraw.enum.ts
+++ src/app/models/enum/withdraw.enum.ts
@@ -5,17 +5,3 @@ export enum withdrawPeriodsEnum {
   QUARTER,
   YEAR
 }
-//
-// export enum withdrawAmountsEnum {
-//   "100",
-//   "200",
-//   "300",
-//   "400",
-//   "500",
-//   "600",
-//   "700",
-//   "800",
-//   "900",
-//   "1000",
-// }
-
