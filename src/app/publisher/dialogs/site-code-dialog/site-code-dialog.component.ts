b270841cd3945dc764330810b553a02e597c2cf9
KonradFak <konrad.fak@10clouds.com>
2018-04-11 08:11:50 +0200

    Block payments and see code possibility for users without confirmed email

--- src/app/publisher/dialogs/site-code-dialog/site-code-dialog.component.ts
+++ src/app/publisher/dialogs/site-code-dialog/site-code-dialog.component.ts
@@ -1,15 +1,32 @@
-import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Store } from '@ngrx/store';
+
+import { User } from 'models/user.model';
+import { AppState } from 'models/app-state.model';
+import { HandleSubscription } from 'common/handle-subscription';
 
 @Component({
   selector: 'app-site-code-dialog',
   templateUrl: './site-code-dialog.component.html',
   styleUrls: ['./site-code-dialog.component.scss']
 })
-export class SiteCodeDialogComponent {
+export class SiteCodeDialogComponent extends HandleSubscription implements OnInit {
+  isEmailConfirmed = false;
+
   constructor(
     public dialogRef: MatDialogRef<SiteCodeDialogComponent>,
+    private store: Store<AppState>,
     @Inject(MAT_DIALOG_DATA) public data
-  ) { }
+  ) {
+    super();
+  }
 
+  ngOnInit() {
+    const userSubcrtiption = this.store.select('state', 'user', 'data')
+      .subscribe((user: User) => {
+        this.isEmailConfirmed = user.isEmailConfirmed;
+      });
+    this.subscriptions.push(userSubcrtiption);
+  }
 }
