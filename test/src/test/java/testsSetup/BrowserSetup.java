0983ae4808846291e6f8d4b6f6ebf2c865e278a9
PawelPodkalicki <ppodkali@gmail.com>
2018-04-11 11:47:38 +0200

    Make "webdriver.chrome.driver" gradle parameter

--- test/src/test/java/testsSetup/BrowserSetup.java
+++ test/src/test/java/testsSetup/BrowserSetup.java
@@ -14,6 +14,10 @@ import tools.Structure;
 import tools.Xml;
 
 public class BrowserSetup {
+  /**
+   * System property defining location of ChromeDriver used by Selenium
+   */
+  private static final String SYS_PROP_WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
 
   protected LoginPage loginPage;
   protected DashboardPopup dashboardPopup;
@@ -28,7 +32,10 @@ public class BrowserSetup {
 
   @BeforeClass
   public void setChrome() {
-    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+    String property = System.getProperty(SYS_PROP_WEBDRIVER_CHROME_DRIVER);
+    if (property == null || "".equals(property)) {
+      System.setProperty(SYS_PROP_WEBDRIVER_CHROME_DRIVER, "C:\\chromedriver.exe");
+    }
     driver = new ChromeDriver();
     driver.manage().window().maximize();
     driver.get("http://localhost:4200/");
