2cd46581c29793b69b3a959a32505ab03112efbf
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-04-17 11:26:48 +0200

    Fixed support email and indent

--- src/app/auth/confirmation/confirmation.component.ts
+++ src/app/auth/confirmation/confirmation.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { appSettings } from 'app-settings';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.scss'],
 })
-export class ConfirmationComponent { }
+export class ConfirmationComponent {
+  supportEmail = appSettings.SUPPORT_EMAIL;
+}
