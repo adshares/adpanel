da946619a9f42fa28861955d48e6a67fbfc59900
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-04-23 20:10:32 +0200

    Improved fonts' RWD, improved tooltips

--- src/app/models/campaign.model.ts
+++ src/app/models/campaign.model.ts
@@ -12,7 +12,7 @@ interface Campaign {
   ads?: Ad[];
   clicks?: number;
   impressions?: number;
-  ctr?: number;
+  CTR?: number;
   averageCPC?: number;
   cost?: number;
   conversions?: number;
@@ -38,7 +38,7 @@ interface Ad {
   size: number;
   clicks: number;
   impressions: number;
-  ctr: number;
+  CTR: number;
   averageCPC: number;
   cost: number;
   budget: number;
