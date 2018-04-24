782d54cb9af9078ce01c086e75b2f57428436c76
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-04-24 11:34:54 +0200

    Code review fixes

---
+++ src/app/models/chart/chart-other.model.ts
@@ -0,0 +1,13 @@
+export interface ChartJsComputedData {
+  labels: string[];
+  datasets: object[];
+}
+
+export interface TooltipItem {
+  datasetIndex: number;
+  index: number;
+  x: number;
+  xLabel: string;
+  y: number;
+  yLabel: number;
+}
