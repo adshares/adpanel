a19799f6cf53e066ea1e5bbd10555bcd87db7ea1
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-05-02 11:05:47 +0200

    Added styling and HTML, mock function and icons

--- src/app/common/components/push-notifications/push-notification/push-notification.component.ts
+++ src/app/common/components/push-notifications/push-notification/push-notification.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-push-notification',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./push-notification.component.scss'],
 })
 export class PushNotificationComponent {
+  // @Input() pushNotification: PushNotification
+  @Input() pushNotification: any;
 
+  dismissPushNotification() {
+
+  }
 }
