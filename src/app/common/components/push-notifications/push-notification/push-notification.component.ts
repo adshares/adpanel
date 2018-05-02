298209fb5c08c06ab48e21c33c15eb39831940b9
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-05-02 09:57:45 +0200

    Setup files for push notifications

---
+++ src/app/common/components/push-notifications/push-notification/push-notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-push-notification',
+  templateUrl: './push-notification.component.html',
+  styleUrls: ['./push-notification.component.scss'],
+})
+export class PushNotificationComponent {
+
+}
---
+++ src/app/common/components/push-notifications/push-notifications.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications',
+  templateUrl: './push-notifications.component.html',
+  styleUrls: ['./push-notifications.component.scss'],
+})
+export class PushNotificationsComponent {
+}
