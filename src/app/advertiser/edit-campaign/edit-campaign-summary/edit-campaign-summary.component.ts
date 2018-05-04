4319cb8cdebc354c7032619554ea62db0caa98f3
KonradFak <konrad.fak@10clouds.com>
2018-05-04 17:14:44 +0200

    Removed unused dependency

--- src/app/advertiser/edit-campaign/edit-campaign-summary/edit-campaign-summary.component.ts
+++ src/app/advertiser/edit-campaign/edit-campaign-summary/edit-campaign-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Router, ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/first';
