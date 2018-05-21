8c67f2886b32f42cfee6aaa3509ef87dc400b4b5
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2018-05-21 17:17:41 +0200

    Auto discover web driver

--- test/pl/adshares/adpanel/setup/DriverProvider.java
+++ test/pl/adshares/adpanel/setup/DriverProvider.java
@@ -4,23 +4,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+
 public class DriverProvider {
 
+  public static final String DRIVERS_DIRECTORY = "build/drivers";
 
   private static WebDriver driver;
 
   private DriverProvider() {
   }
 
+  private static File getWebDriverFile(String name) {
+    File driversDir = new File(DRIVERS_DIRECTORY);
+    if (!driversDir.exists()) {
+      throw new RuntimeException("Cannot find web drivers directory: " + DRIVERS_DIRECTORY);
+    }
+
+    File driverFile = null;
+    for (File file : driversDir.listFiles()) {
+      if (file.getName().startsWith(name) && !file.getName().endsWith(".version")) {
+        driverFile = file;
+        break;
+      }
+    }
+    if (null == driverFile) {
+      throw new RuntimeException("Cannot find web driver '" + name + "' in directory " + DRIVERS_DIRECTORY);
+    }
+
+    return driverFile;
+  }
+
   static WebDriver getWebDriver() {
     if (driver == null) {
-      System.setProperty("webdriver.chrome.driver", "C:\\var\\git\\adpanel\\build\\drivers\\chromedriver-windows-32bit.exe");
+      System.setProperty("webdriver.chrome.driver", getWebDriverFile("chromedriver").getAbsolutePath());
       ChromeOptions chromeOptions = new ChromeOptions();
-//      chromeOptions.setBinary("build/drivers/chromedriver-windows-32bit.exe");
       chromeOptions.addArguments("--start-maximized");
       driver = new ChromeDriver(chromeOptions);
       driver.get("http://panel.ads");
     }
+
     return driver;
   }
 
