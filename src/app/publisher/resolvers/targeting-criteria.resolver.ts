ba9f9ac7d84ffdb03bd9a4848a0ebd7470563773
Kamil Hajduk <kamil.hajduk@adshares.pl>
2018-05-30 14:51:54 +0200

    152 models refactoring route change routing

--- src/app/publisher/resolvers/targeting-criteria.resolver.ts
+++ src/app/publisher/resolvers/targeting-criteria.resolver.ts
@@ -11,7 +11,7 @@ export class TargetingCriteriaResolver implements Resolve<any> {
   constructor(private publisherService: PublisherService) { }
 
   resolve(route: ActivatedRouteSnapshot): Observable<TargetingOption[]> {
-    return this.publisherService.getTargetingCriteria()
+    return this.publisherService.getTargetingCriteria(route.params.id)
       .map((targetingOptions) => prepareTargetingChoices(targetingOptions));
   }
 }
