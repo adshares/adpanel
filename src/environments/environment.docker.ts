2cf2941be211f138f7150af12a737db3683f5c37
Kamil Hajduk <kamil.hajduk@adshares.pl>
2018-06-07 13:24:16 +0200

    152_1 fix wiremcok models

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://panel.ads:8299'
+  apiUrl: 'http://127.0.0.1:8299'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8299'
+  apiUrl: 'http://127.0.0.1:8299'
 };
