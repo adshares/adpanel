cc49a30980e855129e81f9edb8ddc5dd791c4c0e
Tomek Grzechowski <keo@becomewater.com>
2018-06-21 08:27:27 +0200

    fixing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import {Routes, RouterModule, Router} from '@angular/router';
 import {LoginComponent} from "auth/login/login.component";
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/auth/register-confirm/:token', pathMatch: 'full' },
   { path: '', redirectTo: '/admin/dashboard', pathMatch: 'full' },
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' }
 ];
