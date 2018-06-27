863ccd62190edea0bd3418d6a920686373160adf
Kamil Hajduk <kamil.hajduk@adshares.pl>
2018-06-27 10:11:46 +0200

    190 fixed add prefix user for entry data from adserver

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -62,7 +62,6 @@ export class HeaderComponent extends HandleSubscription implements OnInit {
       });
 
     this.subscriptions.push(userFinancialDataSubscription, activeUserTypeSubscription, notificationsTotalSubscription);
-
     this.userDataState = this.store.select('state', 'user', 'data');
 
 
