4f093d9fd24d21f23ed1788f04f4bf0f8d31b6ce
Przemyslaw Furtak <przemyslawf@gmail.com>
2018-10-19 16:32:48 +0200

    Site feature

--- src/app/publisher/edit-site/edit-site-create-ad-units/edit-site-create-ad-units.component.ts
+++ src/app/publisher/edit-site/edit-site-create-ad-units/edit-site-create-ad-units.component.ts
@@ -46,6 +46,7 @@ export class EditSiteCreateAdUnitsComponent extends HandleLeaveEditProcess imple
 
   ngOnInit() {
     this.adUnitSizes = cloneDeep(this.route.snapshot.data.adUnitSizes);
+    console.log(this.adUnitSizes);
     this.adSizesOptions.unshift('Recommended');
     const lastSiteSubscription = this.store.select('state', 'publisher', 'lastEditedSite')
       .first()
