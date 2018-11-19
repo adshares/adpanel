66e969a976d7e95a8664c6aabdca9fc0d1d6a705
PawelPodkalicki <ppodkali@gmail.com>
2018-11-19 15:58:00 +0100

    PAN-126: Fix wallet balance

--- src/app/common/utilities/helpers.ts
+++ src/app/common/utilities/helpers.ts
@@ -1,4 +1,4 @@
-import { TableColumnMetaData } from 'models/table.model';
+import {TableColumnMetaData} from 'models/table.model';
 
 /**
  * Calculates campaign budget per hour in ADS.
@@ -54,6 +54,40 @@ function enumToObjectArray(enumInput) {
   }
 
   return enumNameArrayObject;
+}
+
+function formatMoney(
+  value,
+  precision: number = 11,
+  trim: boolean = false,
+  decimal: string = '.',
+  thousand: string = ','
+): string {
+  const r = trim;
+  const p = Math.max(precision, 2);
+  const d = decimal;
+  const t = thousand;
+  let v = value;
+
+  v = ((v || '0') + '').padStart(11, '0');
+  const l = v.length - 11;
+  const a = v.substr(0, l) || '0';
+  const j = a.length > 3 ? a.length % 3 : 0;
+  let b = Math.round(parseInt((v + '0')
+    .substr(l, p + 1)) / 10)
+    .toString()
+    .padStart(p, '0')
+  ;
+  if (r) {
+    b = b.replace(/([0-9]{2})0+$/, '$1');
+  }
+
+  return (
+    (j ? a.substr(0, j) + t : '') +
+    a.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + t) +
+    d +
+    b
+  );
 }
 
 function isUnixTimePastNow(unixTime): boolean {
@@ -117,6 +151,7 @@ export {
   enumToArray,
   enumToObject,
   enumToObjectArray,
+  formatMoney,
   isUnixTimePastNow,
   selectCompare,
   createInitialArray,
