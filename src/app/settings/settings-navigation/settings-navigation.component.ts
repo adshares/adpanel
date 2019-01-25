188076053b426832c8e1b462b804ff17e8024143
Meskat <marciniszyn.k@gmail.com>
2019-01-25 12:53:49 +0100

    Fix: swap static funds with updated ones

--- src/app/settings/settings-navigation/settings-navigation.component.ts
+++ src/app/settings/settings-navigation/settings-navigation.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 
 import { SessionService } from "app/session.service";
 import { LocalStorageUser, UserAdserverWallet } from 'models/user.model';
+import {Store} from "@ngrx/store";
+import {AppState, SettingsState} from "models/app-state.model";
+import {HandleSubscription} from "common/handle-subscription";
 
 @Component({
   selector: 'app-settings-navigation',
   templateUrl: './settings-navigation.component.html',
   styleUrls: ['./settings-navigation.component.scss'],
 })
-export class SettingsNavigationComponent {
+export class SettingsNavigationComponent extends HandleSubscription {
   adserverWallet: UserAdserverWallet;
+  totalFunds: number;
   user: LocalStorageUser;
   // userRoles: Store<UserRoles>
 
@@ -41,11 +45,19 @@ export class SettingsNavigationComponent {
 
   constructor(
     private session: SessionService,
+    private store: Store<AppState>
   ) {
+    super()
   }
 
   ngOnInit() {
     this.user = this.session.getUser();
     this.adserverWallet = this.user.adserverWallet;
+
+    const userDataStateSubscription = this.store.select('state', 'user', 'settings')
+      .subscribe((state: SettingsState) => {
+        this.totalFunds = state.totalFunds
+      });
+    this.subscriptions.push(userDataStateSubscription);
   }
 }
