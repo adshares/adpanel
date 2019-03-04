c1d30fa44fd30027687629e94c0ad3da2dbaeb93
PawelPodkalicki <38037054+PawelPodkalicki@users.noreply.github.com>
2019-03-04 13:14:03 +0100

    Fix banner preview build prod - inconsistent types (#497)

--- src/app/common/components/banner-preview/banner-preview.component.ts
+++ src/app/common/components/banner-preview/banner-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BannerClassification } from 'models/classifier.model';
 import { Ad } from 'models/campaign.model';
 import { adTypesEnum } from 'models/enum/ad.enum';
@@ -8,13 +8,28 @@ import { adTypesEnum } from 'models/enum/ad.enum';
   templateUrl: './banner-preview.component.html',
   styleUrls: ['./banner-preview.component.scss'],
 })
-export class BannerPreviewComponent {
+export class BannerPreviewComponent implements OnInit {
   @Input() banner: BannerClassification | Ad;
   readonly IFRAME_TITLE: string = 'Banner Preview';
 
+  isBannerInputTypeAd: boolean;
+  bannerUrl: string;
+  bannerHtml: string;
+
   constructor() {
   }
-  
+
+  ngOnInit(): void {
+    if ((<BannerClassification>this.banner).url) {
+      this.isBannerInputTypeAd = false;
+      this.bannerUrl = (<BannerClassification>this.banner).url;
+    } else {
+      this.isBannerInputTypeAd = true;
+      this.bannerUrl = (<Ad>this.banner).imageUrl;
+      this.bannerHtml = (<Ad>this.banner).html;
+    }
+  }
+
   get isImage() {
     return this.banner.type === adTypesEnum.IMAGE;
   }
