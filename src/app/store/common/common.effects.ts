75a8d0543f1fb07748c0d3c968976788d44c963d
Meskat <marciniszyn.k@gmail.com>
2019-03-09 19:43:03 +0100

    SRV-239: Fix earnings settings in admin panel

--- src/app/store/common/common.effects.ts
+++ src/app/store/common/common.effects.ts
@@ -6,6 +6,7 @@ import { CommonService } from 'common/common.service';
 import * as commonActions from './common.actions';
 import * as advertiserActions from "store/advertiser/advertiser.actions";
 import * as publisherActions from "store/publisher/publisher.actions";
+import * as adminActions from "store/admin/admin.actions";
 import { ErrorResponseDialogComponent } from "common/dialog/error-response-dialog/error-response-dialog.component";
 import { MatDialog } from "@angular/material";
 
@@ -30,7 +31,9 @@ export class CommonEffects {
       advertiserActions.UPDATE_CAMPAIGN_STATUS_FAILURE,
       advertiserActions.DELETE_CAMPAIGN_FAILURE,
       advertiserActions.DELETE_CAMPAIGN_FAILURE,
-      publisherActions.ADD_SITE_TO_SITES_FAILURE
+      publisherActions.ADD_SITE_TO_SITES_FAILURE,
+      adminActions.SET_ADMIN_SETTINGS_FAILURE
+
     )
     .map(toPayload)
     .do(payload => {
