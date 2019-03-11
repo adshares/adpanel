0add12a632d840681f817cfc4934036cb74bf4fa
Meskat <marciniszyn.k@gmail.com>
2019-03-11 14:39:21 +0100

    SRV-239: Fix earnings settings in admin panel (#516)

--- src/app/admin/dialogs/set-your-earnings-dialog/set-your-earnings-dialog.component.ts
+++ src/app/admin/dialogs/set-your-earnings-dialog/set-your-earnings-dialog.component.ts
@@ -1,51 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
-import { Subject } from 'rxjs/Subject';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-
 import { HandleSubscription } from 'common/handle-subscription';
 import { AppState } from 'models/app-state.model';
 import { AdminSettings } from 'models/settings.model';
-import { AdminService } from 'admin/admin.service';
-import * as adminActions from 'store/admin/admin.actions';
+import {SetAdminSettings} from 'store/admin/admin.actions';
 
 @Component({
   selector: 'app-set-your-earnings-dialog',
   templateUrl: './set-your-earnings-dialog.component.html',
   styleUrls: ['./set-your-earnings-dialog.component.scss']
 })
+
 export class SetYourEarningsDialogComponent extends HandleSubscription implements OnInit {
   value: number;
-  valueChanged = new Subject<number>();
 
   constructor(
     public dialogRef: MatDialogRef<SetYourEarningsDialogComponent>,
     private store: Store<AppState>,
-    private adminService: AdminService
   ) {
     super();
   }
 
   ngOnInit() {
     const adminStoreSettingsSubscription = this.store.select('state', 'admin', 'settings')
+      .first()
       .subscribe((settings: AdminSettings) => {
         this.value = settings.earnings * 100;
       });
     this.subscriptions.push(adminStoreSettingsSubscription);
-
-    const adminSettingsChangeSubscription = this.valueChanged.pipe(
-      debounceTime(500),
-      distinctUntilChanged()
-    )
-      .subscribe((newValue: number) => {
-        this.adminService.setAdminSettings({earnings: newValue})
-          .subscribe((newSettings) => this.store.dispatch(new adminActions.SetAdminSettings(newSettings)));
-      });
-    this.subscriptions.push(adminSettingsChangeSubscription);
   }
 
   updateValue(newValue) {
-    this.valueChanged.next(newValue);
+    this.value = newValue;
+  }
+
+  saveSettings(): void {
+    this.dialogRef.close();
+    this.store.dispatch(new SetAdminSettings({earnings: this.value}))
   }
 }
