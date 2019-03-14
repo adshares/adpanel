dc1e8b4516487cabcd6b824dffa6634701485154
Meskat <marciniszyn.k@gmail.com>
2019-03-14 13:46:31 +0100

    PAN-276: Fix condition for classification banners filtering (#522)

--- src/app/publisher/publisher.service.ts
+++ src/app/publisher/publisher.service.ts
@@ -84,7 +84,8 @@ export class PublisherService {
       params = {
         ...params,
         ...filtering.status,
-        sizes: !!filtering.sizes ? JSON.stringify(filtering.sizes) : JSON.stringify(possibleSizes)
+        sizes: (!!filtering.sizes && filtering.sizes.length) ? JSON.stringify(filtering.sizes)
+          : JSON.stringify(possibleSizes)
       };
     }
 
