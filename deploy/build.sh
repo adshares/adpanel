102960dde48d8529a0414247cc1a31464b8f975c
Paweł Zaremba <pawzar@gmail.com>
2019-03-15 17:17:27 +0100

    deploy

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
-set -e
-
-source ${1:-$(dirname $(readlink -f "$0"))/bin}/_functions.sh
+source ${1}/_functions.sh || { echo "Missing source path (first param)."; exit 1 }
 [[ -z ${2:-""} ]] || cd $2
 [[ -z ${3:-".env"} ]] || set -a && source .env && set +a
-
-ADSERVER_URL_FROM_CMD=${1:-http://localhost:8001}
 
 GIT_TAG=$(git tag -l --points-at HEAD | head -n 1)
 GIT_HASH="#"$(git rev-parse --short HEAD)
 
 export APP_VERSION=${APP_VERSION:-${GIT_TAG:-${GIT_HASH}}}
+export APP_PORT=${APP_PORT:-8002}
+
 export APP_PROD=${APP_PROD:-true}
 export ADSERVER_URL=${ADSERVER_URL:-${ADSERVER_URL_FROM_CMD}}
 export DEV_XDEBUG=${DEV_XDEBUG:-false}
 export APP_ENV=${APP_ENV:-prod}
-export APP_PORT=${APP_PORT:-8002}
 
 envsubst < src/environments/environment.ts.template | tee src/environments/environment.${APP_ENV}.ts
 
