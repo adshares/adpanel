d347a9cff08f53dbde0e85e80eb2d2437e14d4d3
Meskat <marciniszyn.k@gmail.com>
2019-03-27 14:29:41 +0100

    PAN-303: Fix setting commission input behavior: - added hammerjs -fixed formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { GestureConfig } from '@angular/material';
+
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -6,34 +8,33 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppCommonModule } from './common/common.module';
+import { AppCommonModule } from 'common/common.module';
 import { ApiModule } from './api/api.module';
-import { AuthModule } from './auth/auth.module';
-import { AdvertiserModule } from './advertiser/advertiser.module';
-import { AdminModule } from './admin/admin.module';
-import { PublisherModule } from './publisher/publisher.module';
-import { SettingsModule } from './settings/settings.module';
+import { AuthModule } from 'auth/auth.module';
+import { AdvertiserModule } from 'advertiser/advertiser.module';
+import { AdminModule } from 'admin/admin.module';
+import { PublisherModule } from 'publisher/publisher.module';
+import { SettingsModule } from 'settings/settings.module';
 import { AppComponent } from './app.component';
 
 import { environment } from 'environments/environment';
 
-import { RequestInterceptor } from './common/request.interceptor';
+import { RequestInterceptor } from 'common/request.interceptor';
 
-import { AdvertiserService } from './advertiser/advertiser.service';
-import { PublisherService } from './publisher/publisher.service';
-import { SettingsService } from './settings/settings.service';
-import { AdminService } from './admin/admin.service';
+import { AdvertiserService } from 'advertiser/advertiser.service';
+import { PublisherService } from 'publisher/publisher.service';
+import { SettingsService } from 'settings/settings.service';
+import { AdminService } from 'admin/admin.service';
 import { AuthService } from 'app/auth.service';
 import { SessionService } from 'app/session.service';
 
-import { AdvertiserEffects } from './store/advertiser/advertiser.effects';
-import { PublisherEffects } from './store/publisher/publisher.effects';
-import { SettingsEffects } from './store/settings/settings.effects';
-import { AdminEffects } from './store/admin/admin.effects';
-import { CommonEffects } from './store/common/common.effects';
+import { AdvertiserEffects } from 'store/advertiser/advertiser.effects';
+import { PublisherEffects } from 'store/publisher/publisher.effects';
+import { SettingsEffects } from 'store/settings/settings.effects';
+import { AdminEffects } from 'store/admin/admin.effects';
+import { CommonEffects } from 'store/common/common.effects';
 
-import { reducer } from './store/index';
-
+import { reducer } from 'store/index';
 import { ConfirmResponseDialogComponent } from "common/dialog/confirm-response-dialog/confirm-response-dialog.component";
 import { ErrorResponseDialogComponent } from "common/dialog/error-response-dialog/error-response-dialog.component";
 const appModules = [
@@ -72,6 +73,7 @@ const appModules = [
       useClass: RequestInterceptor,
       multi: true
     },
+    { provide:   HAMMER_GESTURE_CONFIG, useClass: GestureConfig },
     AdvertiserService,
     PublisherService,
     SettingsService,
