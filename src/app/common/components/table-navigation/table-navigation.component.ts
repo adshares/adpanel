6ec8ec9aa38079288158ea223ad370ae9bac0346
PawelPodkalicki <38037054+PawelPodkalicki@users.noreply.github.com>
2019-04-02 12:32:05 +0200

    PAN-330 Rename site cost to revenue (#579)

--- src/app/common/components/table-navigation/table-navigation.component.ts
+++ src/app/common/components/table-navigation/table-navigation.component.ts
@@ -41,7 +41,7 @@ export class TableNavigationComponent implements OnInit {
   siteListNavigationItems = [
     {title: 'Status', columnWidth: 'col-xs-1', keys: ['status'], sortAsc: true},
     {title: 'Website Name', columnWidth: 'col-xs-4', keys: ['name'], sortAsc: true},
-    {title: 'Est. Revenue', columnWidth: 'col-xs-1', keys: ['cost'], sortAsc: true},
+    {title: 'Est. Revenue', columnWidth: 'col-xs-1', keys: ['revenue'], sortAsc: true},
     {title: 'Clicks', columnWidth: 'col-xs-1', keys: ['clicks'], sortAsc: true},
     {title: 'Views', columnWidth: 'col-xs-1', keys: ['impressions'], sortAsc: true},
     {title: 'CTR', columnWidth: 'col-xs-1', keys: ['ctr'], sortAsc: true},
