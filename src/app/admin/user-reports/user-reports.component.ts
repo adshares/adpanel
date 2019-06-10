8c59d03c4a3e2bd826ef56653aa0a8870d0cf753
Meskat <marciniszyn.k@gmail.com>
2019-06-10 15:40:34 +0200

    PAN-406: User reports time filters

--- src/app/admin/user-reports/user-reports.component.ts
+++ src/app/admin/user-reports/user-reports.component.ts
@@ -3,6 +3,7 @@ import { HandleSubscription } from 'common/handle-subscription';
 import * as moment from 'moment';
 import { AdminService } from 'admin/admin.service';
 import { downloadCSVFile } from 'common/utilities/helpers';
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-user-reports',
@@ -10,34 +11,39 @@ import { downloadCSVFile } from 'common/utilities/helpers';
   styleUrls: ['./user-reports.component.scss'],
 })
 export class UserReportsComponent extends HandleSubscription implements OnInit {
-  from: string;
-  to: string;
+  from: FormControl = new FormControl();
+  to: FormControl = new FormControl();
+  today = new Date();
 
   constructor(private adminService: AdminService) {
     super();
   }
 
   ngOnInit() {
-    this.from = moment().subtract(30, 'days').startOf('day').format();
-    this.to = moment().endOf('day').format();
+    this.from.setValue(moment().subtract(30, 'days').startOf('day').format());
+    this.to.setValue(moment().endOf('day').format());
   }
 
   load(timespan) {
-    this.from = moment(timespan.from).startOf('day').format();
-    this.to = moment(timespan.to).endOf('day').format();
   }
 
+
+
   getReportAdvertisers() {
-    this.adminService.getReportAdvertisers(this.from, this.to)
+    const from =  moment(this.from.value).format();
+    const to =  moment(this.to.value).format();
+    this.adminService.getReportAdvertisers(from, to)
       .subscribe((data) => {
-        downloadCSVFile(data, this.from, this.to);
+        downloadCSVFile(data, from, to);
       });
   }
 
   getReportPublishers() {
-    this.adminService.getReportPublishers(this.from, this.to)
+    const from =  moment(this.from.value).format();
+    const to =  moment(this.to.value).format();
+    this.adminService.getReportPublishers(from, to)
       .subscribe((data) => {
-        downloadCSVFile(data, this.from, this.to);
+        downloadCSVFile(data, from, to);
       });
   }
 }
