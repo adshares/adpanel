790cc0de2eddb402def26f0716ffc265f3892f6d
PawelPodkalicki <ppodkali@gmail.com>
2019-12-06 15:04:32 +0100

    Gray out wallet data when check fails

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -22,6 +22,7 @@ interface User {
   password: string;
   apiToken?: string;
   adserverWallet: UserAdserverWallet;
+  isAdserverWalletValid: boolean;
   exchangeRate: ExchangeRate | null;
   uuid: string;
   referralId?: string;
