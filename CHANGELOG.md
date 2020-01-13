eea1e022560ba08f0cc500a476965d5ae8ae9de3
PawelPodkalicki <ppodkali@gmail.com>
2020-01-13 11:16:57 +0100

    Limit chart requests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [1.4.8] - 2020-01-03
 ### Changed
 - Default direct link value
+- Limit chart requests
 ### Fixed
 - Long direct links
 
--- src/app/publisher/dashboard/dashboard.component.ts
+++ src/app/publisher/dashboard/dashboard.component.ts
@@ -70,7 +70,7 @@ export class DashboardComponent extends HandleSubscription implements OnInit {
       )
       .subscribe(data => {
         this.barChartData[0].data = data.values;
-        this.barChartData[0].currentSeries = this.currentChartFilterSettings.currentSeries.label;
+        this.barChartData[0].currentSeries = chartFilterSettings.currentSeries.label;
         this.barChartLabels = data.timestamps.map(item => moment(item).format());
         this.barChartValue = data.total;
         this.barChartDifference = data.difference;
