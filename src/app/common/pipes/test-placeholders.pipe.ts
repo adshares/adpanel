0d3dcbe6621ed1118b207483872456986fa9beed
PawelPodkalicki <ppodkali@gmail.com>
2020-04-22 23:01:49 +0200

    clean code

--- src/app/common/pipes/test-placeholders.pipe.ts
+++ src/app/common/pipes/test-placeholders.pipe.ts
@@ -2,16 +2,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'testPlaceholders'})
 export class TestPlaceholdersPipe implements PipeTransform {
-
-  readonly PLACEHOLDERS = [
-    {id: '{aid}', description: 'server id'},
-    {id: '{pid}', description: 'publisher id'},
-    {id: '{sid}', description: 'site id'},
-    {id: '{zid}', description: 'zone id'},
-    {id: '{cid}', description: 'case id'},
-    {id: '{bid}', description: 'banner id'},
-  ];
-
   transform(url: string) {
     return url.replace(/{aid}/g, 'test_server_id')
       .replace(/{pid}/g, 'test_publisher')
