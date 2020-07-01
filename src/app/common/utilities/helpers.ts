452983543bc049cee2cc89ee8b82ea66d13b7add
PawelPodkalicki <ppodkali@gmail.com>
2020-07-01 16:18:45 +0200

    Cut direct ad content anchor and fix bid strategies

--- src/app/common/utilities/helpers.ts
+++ src/app/common/utilities/helpers.ts
@@ -39,6 +39,11 @@ function cloneDeep(target) {
   return JSON.parse(JSON.stringify(target));
 }
 
+function cutDirectAdSizeAnchor(url: string): string {
+  const index = url.lastIndexOf('#');
+  return -1 === index ? url: url.substring(0, index);
+}
+
 function enumToArray(enumInput) {
   const enumNamesArray: string[] = [];
 
@@ -125,10 +130,6 @@ function formatMoney(
 function isUnixTimePastNow(unixTime): boolean {
   const nowUnix = (+new Date) / 1000 | 0;
   return unixTime < nowUnix;
-}
-
-function selectCompare(value, nextValue): boolean {
-  return (value && nextValue) ? (value.id === nextValue.id) : (value === nextValue);
 }
 
 function createInitialArray(element, count) {
@@ -295,12 +296,12 @@ export {
   calcCampaignBudgetPerDay,
   calcCampaignBudgetPerHour,
   cloneDeep,
+  cutDirectAdSizeAnchor,
   enumToArray,
   enumToObject,
   enumToObjectArray,
   formatMoney,
   isUnixTimePastNow,
-  selectCompare,
   createInitialArray,
   sortArrayByKeys,
   adjustCampaignStatus,
