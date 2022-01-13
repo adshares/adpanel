d18f0c04c08c92fc744e63766de140f0b325eb44
Maciej Pilarczyk <m.pilarczyk@outlook.com>
2022-01-13 15:07:00 +0100

    Sorting add sizes

--- src/app/advertiser/edit-campaign/edit-campaign-create-ads/edit-campaign-create-ads.component.ts
+++ src/app/advertiser/edit-campaign/edit-campaign-create-ads/edit-campaign-create-ads.component.ts
@@ -55,7 +55,7 @@ interface UploadingFile {
 export class EditCampaignCreateAdsComponent extends HandleSubscription implements OnInit {
   adForms: FormGroup[] = [];
   adTypes: string[] = enumToArray(adTypesEnum);
-  displayAdSizes: string[] = enumToArray(displayAdSizesEnum);
+  displayAdSizes: string[];
   popAdSizes: string[] = enumToArray(popAdSizesEnum);
   adStatusesEnum = adStatusesEnum;
   ads: Ad[] = [];
@@ -88,6 +88,11 @@ export class EditCampaignCreateAdsComponent extends HandleSubscription implement
     private matDialog: MatDialog,
   ) {
     super();
+    this.displayAdSizes = enumToArray(displayAdSizesEnum).sort((a, b) => {
+      const sizesA = a.split('x').map(val => parseInt(val));
+      const sizesB = b.split('x').map(val => parseInt(val));
+      return sizesA[0] === sizesB[0] ? sizesA[1] - sizesB[1] : sizesA[0] - sizesB[0];
+    });
   }
 
   ngOnInit() {
