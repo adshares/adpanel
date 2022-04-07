7d030cb7c382c2713c05a3c2fce0626e55faec6d
Paweł Podkalicki <38037054+PawelPodkalicki@users.noreply.github.com>
2022-04-07 20:45:23 +0200

    Change wallet login visibility (#879)

--- src/app/models/enum/link.enum.ts
+++ src/app/models/enum/link.enum.ts
@@ -2,5 +2,9 @@
 export const ADD_UNIT_CRYPTOVOXELS = 'https://adshar.es/cryptovoxels'
 export const ADD_UNIT_DECENTRALAND = 'https://adshar.es/decentraland'
 export const ADD_UNIT_DECENTRALAND_SMART = 'https://adshar.es/dclsmart'
+// List of ad units available in the network
 export const ADSHARES_UNITS = 'https://adshares.net/units'
+// Adshares wallet installation instruction
 export const ADSHARES_WALLET = 'https://adshares.net/wallet'
+// Metamask wallet installation instruction
+export const METAMASK_WALLET = 'https://metamask.io/'
