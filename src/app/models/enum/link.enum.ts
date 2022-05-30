c1999c8c2f660d01f0d674df89d62ab31efb297f
Alex <67001075+szych111@users.noreply.github.com>
2022-05-30 11:25:05 +0200

    Update src/app/models/enum/link.enum.ts
    Co-authored-by: Maciej Pilarczyk <m.pilarczyk@outlook.com>

--- src/app/models/enum/link.enum.ts
+++ src/app/models/enum/link.enum.ts
@@ -11,4 +11,4 @@ export const DECENTRALAND_BUILDER = 'https://builder.decentraland.org/'
 // Metamask wallet installation instruction
 export const METAMASK_WALLET = 'https://metamask.io/'
 // wiki guide on how to add an html ad
-export const WIKI_HTML_AD = 'https://github.com/adshares/adserver/wiki/How-to-add-html-banner'
+export const WIKI_HTML_AD = 'https://adshar.es/howtohtmlbanner'
