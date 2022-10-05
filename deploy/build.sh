5897a37593dd479c2bd22d91860a2252de71584f
PawełPodkalicki <ppodkali@gmail.com>
2022-10-05 10:25:32 +0200

    Extract reload from build script

--- deploy/build.sh
+++ deploy/build.sh
@@ -33,37 +33,4 @@ else
 fi
 if [ $? -ne 0 ]; then exit 1; fi
 
-#DEFAULT_DIRECTORY -> BUILD_DIRECTORY -> TARGET_DIRECTORY
-BUILD_DIRECTORY=dist_tmp
-TARGET_DIRECTORY=dist
-## reload
-if [ -d $BUILD_DIRECTORY ]
-then
-  rm -rf $BUILD_DIRECTORY
-fi
-cp -r $DEFAULT_DIRECTORY $BUILD_DIRECTORY
-
-if [[ -n ${BRAND_ASSETS_DIR:-""} ]]
-then
-  if [[ -d ${BRAND_ASSETS_DIR} ]]
-  then
-    echo "Copying brand assets from ${BRAND_ASSETS_DIR}"
-    cp -fr "${BRAND_ASSETS_DIR}"/* $BUILD_DIRECTORY/
-  else
-    echo "Brand assets directory ${BRAND_ASSETS_DIR} doesn't exist."
-  fi
-fi
-
-INDEX_TEMPLATE=src/index.html.template
-INDEX_FILE=$BUILD_DIRECTORY/index.html
-ROBOTS_FILE=$BUILD_DIRECTORY/robots.txt
-bin/build-index-html.sh $INDEX_TEMPLATE $INDEX_FILE $ROBOTS_FILE
-if [ $? -ne 0 ]; then exit 1; fi
-
-envsubst < info.json.template | tee $BUILD_DIRECTORY/info.json
-
-if [ -d $TARGET_DIRECTORY ]
-then
-  rm -rf $TARGET_DIRECTORY
-fi
-mv $BUILD_DIRECTORY $TARGET_DIRECTORY
+deploy/reload.sh
