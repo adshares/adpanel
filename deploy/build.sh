b40d88418972115ee97f9c885b79f6af85768a3c
PawełPodkalicki <ppodkali@gmail.com>
2022-10-05 13:56:15 +0200

    Build to dist_tmp

--- deploy/build.sh
+++ deploy/build.sh
@@ -20,17 +20,24 @@ envsubst < src/environments/environment.ts.template | tee src/environments/envir
 yarn install
 if [ $? -ne 0 ]; then exit 1; fi
 
+#BUILD_DIRECTORY -> DEFAULT_DIRECTORY
+BUILD_DIRECTORY=dist_tmp
 DEFAULT_DIRECTORY=dist_default
 if [[ ${APP_ENV} == 'dev' ]]
 then
-  yarn build --configuration=dev --output-path=$DEFAULT_DIRECTORY
+  yarn build --configuration=dev --output-path=$BUILD_DIRECTORY
 elif [[ ${APP_ENV} == 'prod' ]]
 then
-  yarn build --configuration=prod --output-path=$DEFAULT_DIRECTORY
+  yarn build --configuration=prod --output-path=$BUILD_DIRECTORY
 else
   echo "ERROR: Unsupported environment ($APP_ENV)."
   exit 1
 fi
 if [ $? -ne 0 ]; then exit 1; fi
 
+if [ -d $DEFAULT_DIRECTORY ]
+then
+  rm -rf $DEFAULT_DIRECTORY
+fi
+mv $BUILD_DIRECTORY $DEFAULT_DIRECTORY
 deploy/reload.sh
