079a5ab9a3f357de8500138da217f62c3126ede8
MykolaZhura <nikoyea14@gmail.com>
2023-01-18 11:37:15 +0100

    /home/mykola/adshares/adpanel/src/app/common/clickOutside.directive.ts   4:13  error  The selector should start with one of these prefixes: "app" (https://angular.io/guide/styleguide#style-02-08)  @angular-eslint/directive-selector

--- src/app/common/clickOutside.directive.ts
+++ src/app/common/clickOutside.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, ElementRef, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
   selector: '[clickOutside]',
 })
 export class ClickOutsideDirective {
