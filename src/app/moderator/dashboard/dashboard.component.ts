77340cf336474c7848db5ada727a3acfcbdbe6ec
PawełPodkalicki <ppodkali@gmail.com>
2023-01-26 10:46:19 +0100

    Add link to ACP for moderator

--- src/app/moderator/dashboard/dashboard.component.ts
+++ src/app/moderator/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { faGears } from '@fortawesome/free-solid-svg-icons';
 import { HandleSubscriptionComponent } from 'common/handle-subscription.component';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent extends HandleSubscriptionComponent {}
+export class DashboardComponent extends HandleSubscriptionComponent {
+  readonly faGears = faGears;
+  adControllerUrl = environment.adControllerUrl;
+}
--- src/app/moderator/moderator.module.ts
+++ src/app/moderator/moderator.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ModeratorRoutingModule } from './moderator-routing.module';
 import { ModeratorGuard } from './moderator-guard.service';
 import { AppCommonModule } from 'common/common.module';
 import { DashboardComponent } from 'moderator/dashboard/dashboard.component';
 
 @NgModule({
-  imports: [AppCommonModule, ModeratorRoutingModule],
+  imports: [AppCommonModule, ModeratorRoutingModule, FontAwesomeModule],
   providers: [ModeratorGuard],
   declarations: [DashboardComponent],
 })
