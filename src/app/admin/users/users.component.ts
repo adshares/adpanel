df294254b0904ab7dcec40f16fce9a7fffd0a701
MykolaZhura <nikoyea14@gmail.com>
2023-01-27 22:39:21 +0100

    remove comments

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-// import { HandleSubscriptionComponent } from 'common/handle-subscription.component';
 
 @Component({
   selector: 'app-users',
@@ -7,39 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent {
-  constructor() {
-    // super();
-  }
-  // links = [
-  //   {
-  //     label: 'Users',
-  //     path: './all',
-  //     queryParams: JSON.parse(localStorage.getItem('usersQueryParams')),
-  //   },
-  //   {
-  //     label: 'Publishers',
-  //     path: './publishers',
-  //     queryParams: JSON.parse(localStorage.getItem('publishersQueryParams')),
-  //   },
-  //   {
-  //     label: 'Advertisers',
-  //     path: './advertisers',
-  //     queryParams: JSON.parse(localStorage.getItem('advertisersQueryParams')),
-  //   },
-  // ];
-  //
-  // onTabClick() {
-  //   const localStorageQueryParamsForUsers = JSON.parse(localStorage.getItem('usersQueryParams'));
-  //   const localStorageQueryParamsForPublishers = JSON.parse(localStorage.getItem('publishersQueryParams'));
-  //   const localStorageQueryParamsForAdvertisers = JSON.parse(localStorage.getItem('advertisersQueryParams'));
-  //   this.links.forEach(link => {
-  //     if (link.label === 'Users') {
-  //       link.queryParams = localStorageQueryParamsForUsers;
-  //     } else if (link.label === 'Publishers') {
-  //       link.queryParams = localStorageQueryParamsForPublishers;
-  //     } else if (link.label === 'Advertisers') {
-  //       link.queryParams = localStorageQueryParamsForAdvertisers;
-  //     }
-  //   });
-  // }
+  constructor() {}
 }
