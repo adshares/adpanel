8d775acba05793c747db7e4ecb96d686c400d7ce
MykolaZhura <nikoyea14@gmail.com>
2023-01-31 17:21:16 +0100

    styling uuid banner in account settings

--- src/app/settings/general-settings/account-settings/account-settings.component.ts
+++ src/app/settings/general-settings/account-settings/account-settings.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { SessionService } from '../../../session.service';
 import { User } from 'models/user.model';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { ShowSuccessSnackbar } from 'store/common/common.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from 'models/app-state.model';
 
 @Component({
   selector: 'app-account-settings',
@@ -9,8 +13,18 @@ import { User } from 'models/user.model';
 })
 export class AccountSettingsComponent {
   user: User;
+  faCopy = faCopy;
 
-  constructor(private session: SessionService) {
+  constructor(private session: SessionService, private store: Store<AppState>) {
     this.user = this.session.getUser();
+  }
+
+  copyUserId(): void {
+    navigator.clipboard
+      .writeText(this.user.uuid)
+      .then(() => {
+        this.store.dispatch(new ShowSuccessSnackbar('Copied!'));
+      })
+      .catch(error => console.log(error));
   }
 }
