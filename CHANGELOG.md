780912e6ef4be43d97c51832ef883b69f30b5f7d
MykolaZhura <nikoyea14@gmail.com>
2023-02-01 14:23:47 +0100

    fix review remarks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,17 +6,14 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
-
 ### Added
 - Displaying user id on account settings section
-
 ### Changed
 - Total rebuild of panel look (color palette, elements sizes, arrangement of elements)
 - Font change to Montserrat 
 - Improved navigation
 - Update labels
 - Font awesome icons instead of image assets
-
 ### Fixed
 - Fix external links
 
--- src/app/models/enum/link.enum.ts
+++ src/app/models/enum/link.enum.ts
@@ -14,9 +14,15 @@ export const ADSHARES_WALLET = 'https://adshares.net/wallet';
 export const ADVERTISER_INSTRUCTION_LINK =
   'https://docs.adshares.net/adserver/how-to-use-advertising-ecosystem.html#how-to-use-the-adserver-instruction-for-advertisers';
 
-// Instruction for publisher
-export const PUBLISHER_INSTRUCTION_LINK =
-  'https://docs.adshares.net/adserver/how-to-use-advertising-ecosystem.html#how-to-use-the-adserver-instruction-for-publishers';
+// available adzone options
+export const AVAILABLE_ADZONE_OPTIONS = 'https://docs.adshares.net/adserver/list-of-available-adzone-options.html';
+
+// circumvent adblocks instruction
+export const CIRCUMVENT_ADBLOCKS_INSTRUCTION =
+  'https://docs.adshares.net/adserver/serve-ad-zone-js-code-locally-to-circumvent-adblocks.html';
+
+// conversions description
+export const CONVERSIONS_DESCRIPTION = 'https://docs.adshares.net/adserver/conversions.html';
 
 // Decentraland builder
 export const DECENTRALAND_BUILDER = 'https://builder.decentraland.org/';
@@ -27,22 +33,16 @@ export const GET_ADS_FAQ = 'https://adshar.es/getADS';
 // Metamask wallet installation instruction
 export const METAMASK_WALLET = 'https://metamask.io/';
 
-// wiki guide on how to add an html ad
-export const WIKI_HTML_AD = 'https://adshar.es/howtohtmlbanner';
-
-// withdraw ADS faq
-export const WITHDRAW_ADS_FAQ = 'https://adshar.es/withdrawADS';
-
-// circumvent adblocks instruction
-export const CIRCUMVENT_ADBLOCKS_INSTRUCTION =
-  'https://docs.adshares.net/adserver/serve-ad-zone-js-code-locally-to-circumvent-adblocks.html';
-
-// available adzone options
-export const AVAILABLE_ADZONE_OPTIONS = 'https://docs.adshares.net/adserver/list-of-available-adzone-options.html';
+// Instruction for publisher
+export const PUBLISHER_INSTRUCTION_LINK =
+  'https://docs.adshares.net/adserver/how-to-use-advertising-ecosystem.html#how-to-use-the-adserver-instruction-for-publishers';
 
 // setup fallback backfill code instruction
 export const SETUP_FALLBACK_BACKFILL_CODE_INSTRUCTION =
   'https://docs.adshares.net/adserver/how-to-setup-fallback-backfill-code.html';
 
-// conversions description
-export const CONVERSIONS_DESCRIPTION = 'https://docs.adshares.net/adserver/conversions.html';
+// wiki guide on how to add an html ad
+export const WIKI_HTML_AD = 'https://adshar.es/howtohtmlbanner';
+
+// withdraw ADS faq
+export const WITHDRAW_ADS_FAQ = 'https://adshar.es/withdrawADS';
