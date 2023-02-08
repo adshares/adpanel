74d41dc91abfa078e7eacbf186806f76248dbafb
MykolaZhura <nikoyea14@gmail.com>
2023-02-08 11:26:03 +0100

    set condition to roles button in right place

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { HandleSubscriptionComponent } from 'common/handle-subscription.component';
 import { AddFundsDialogComponent } from 'common/dialog/add-funds-dialog/add-funds-dialog.component';
-import { userRolesEnum, UserRole } from 'models/enum/user.enum';
+import { userRolesEnum } from 'models/enum/user.enum';
 import { AuthService } from 'app/auth.service';
 import { SessionService } from 'app/session.service';
 import { Store } from '@ngrx/store';
@@ -12,14 +12,14 @@ import { User } from 'models/user.model';
 import { environment } from 'environments/environment';
 import { SetUser } from 'store/auth/auth.actions';
 import { CODE, CRYPTO } from 'common/utilities/consts';
-import { faChevronDown, faPlusCircle, faGears } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faGears, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import {
-  faUser,
   faComments,
+  faCreditCard,
   faEnvelope,
   faLifeRing,
   faPaperPlane,
-  faCreditCard,
+  faUser,
 } from '@fortawesome/free-regular-svg-icons';
 import { ServerOptionsService } from 'common/server-options.service';
 
@@ -41,7 +41,7 @@ export class HeaderComponent extends HandleSubscriptionComponent implements OnIn
   userLabel: string;
   activeUserType: number;
   userRolesEnum = userRolesEnum;
-  userRole = UserRole;
+  userRole: string;
   settingsMenuOpen = false;
   helpMenuOpen = false;
   rolesMenuOpen = false;
@@ -111,6 +111,8 @@ export class HeaderComponent extends HandleSubscriptionComponent implements OnIn
         break;
     }
 
+    this.userRole = accountType;
+
     const userDataStateSubscription = this.store.select('state', 'user', 'data').subscribe((data: User) => {
       this.totalFunds = data.adserverWallet.totalFunds;
       this.isTotalFundsValid = data.isAdserverWalletValid;
@@ -139,24 +141,6 @@ export class HeaderComponent extends HandleSubscriptionComponent implements OnIn
 
   openAddFundsDialog() {
     this.dialog.open(AddFundsDialogComponent);
-  }
-
-  returnUserType() {
-    switch (this.activeUserType) {
-      case 0:
-        return this.userRole.ADMIN;
-
-      case 1:
-        return this.userRole.MODERATOR;
-      case 2:
-        return this.userRole.AGENCY;
-      case 3:
-        return this.userRole.ADVERTISER;
-      case 4:
-        return this.userRole.PUBLISHER;
-      default:
-        return 'unknown role';
-    }
   }
 
   logout() {
