4695bfeb5368bc5b079703203a205b4c36fc9d0d
Mykola <73442621+Niko-Yea@users.noreply.github.com>
2023-02-14 12:50:43 +0100

    Landing page links (#1001)
    * Add landing page link to logo in header
    Add landing page link to logo in login page
    Add link 'Powered by Adshares'
    
    * add logo alt description

--- src/app/common/info/info-page/info-page.component.ts
+++ src/app/common/info/info-page/info-page.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from 'environments/environment';
+import { take } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { AppState } from 'models/app-state.model';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -7,12 +10,24 @@ import { environment } from 'environments/environment';
   templateUrl: './info-page.component.html',
   styleUrls: ['./info-page.component.scss'],
 })
-export class InfoPageComponent {
+export class InfoPageComponent implements OnInit {
   source: string;
   version: string;
+  landingPageUrl: string = null;
+  name: string = null;
 
-  constructor() {
+  constructor(private store: Store<AppState>) {
     this.source = environment.name.toLowerCase().replace(/\s+/, '-');
     this.version = environment.version;
+  }
+
+  ngOnInit() {
+    this.store
+      .select('state', 'common', 'info')
+      .pipe(take(1))
+      .subscribe(info => {
+        this.landingPageUrl = info.landingUrl;
+        this.name = info.name;
+      });
   }
 }
