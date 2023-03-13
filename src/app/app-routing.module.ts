401f6df1f76134c4f817090c896f9969aaf94144
PawełPodkalicki <ppodkali@gmail.com>
2023-03-13 11:00:58 +0100

    move 503 component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ServiceUnavailableComponent } from 'common/info/service-unavailable/service-unavailable.component';
 
 const appRoutes: Routes = [
-  { path: '503', component: ServiceUnavailableComponent, pathMatch: 'full' },
   { path: 'preview', redirectTo: '/auth/login', pathMatch: 'full' },
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
   {
--- src/app/app4xx-routing.module.ts
+++ src/app/app4xx-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error4xxComponent } from 'common/info/page-not-found/error4xx.component';
+import { ServiceUnavailableComponent } from 'common/info/service-unavailable/service-unavailable.component';
 import { HTTP_FORBIDDEN, HTTP_NOT_FOUND } from 'common/utilities/codes';
 
 const appRoutes: Routes = [
   { path: '403', component: Error4xxComponent, data: { code: HTTP_FORBIDDEN } },
   { path: '404', component: Error4xxComponent, data: { code: HTTP_NOT_FOUND } },
+  { path: '503', component: ServiceUnavailableComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '/404' },
 ];
 
